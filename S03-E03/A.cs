d738c9e96c470827d98136925dcda231012cd490
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2022-11-09 23:26:47 -0300

    add: resolução de exercícios da seção 3: E01, E02 e E03

---
+++ S03-E03/A.cs
@@ -0,0 +1,12 @@
+﻿namespace S03_E03
+{
+    static class A
+    {
+        public static void AMain()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("SOMA = " + (a + b));
+        }
+    }
+}
---
+++ S03-E03/B.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace S03_E03
+{
+    static class B
+    {
+        public static void BMain()
+        {
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = 3.14159 * (Math.Pow(raio, 2));
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
