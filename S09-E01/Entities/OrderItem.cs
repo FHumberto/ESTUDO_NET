900c1b025d9f8347c00ae66265e5587c41c38849
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2022-11-21 08:01:06 -0300

    add: resolução do excercício da seção 9: E01

---
+++ S09-E01/Entities/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace S09_E01.Entities
+{
+    internal class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product? Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Product.Name}, Quantity: {Quantity}, SubTotal: ${SubTotal().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
