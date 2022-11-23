b37ddebf270b5a6d4e1fb0510b52785de89ca8e4
Humberto Guedes <franciscohguedes@gmail.com>
2022-11-22 22:06:23 -0300

    add: resolução dos exercícios da seção 10: E00, E01.

---
+++ S10-E00/OutsourcedEmployee.cs
@@ -0,0 +1,20 @@
+﻿namespace S10_E00
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AddicionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        { }
+
+        public OutsourcedEmployee(string? name, int hours, double valuePerHour, double adicionalCharge) : base(name, hours, valuePerHour)
+        {
+            AddicionalCharge = adicionalCharge;
+        }
+
+        public override sealed double Payment()
+        {
+            return base.Payment() + 1.1 * AddicionalCharge;
+        }
+    }
+}
