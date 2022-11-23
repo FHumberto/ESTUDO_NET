92a100d66ed445f0d92a4f7d04eab1073dc640e8
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2022-11-22 22:06:23 -0300

    add: resolução dos exercícios da seção 10: E00, E01.

---
+++ S10-E00/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace S10_E00
+{
+    internal class Employee
+    {
+        public string? Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        { }
+
+        public Employee(string? name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - $ {Payment().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
