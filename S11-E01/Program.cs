fa701af923a3b092a31c899d5b18b572cdbc0685
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2022-11-25 14:05:51 -0300

    add: resolução do exercício da seção 11: E01.

---
+++ S11-E01/Program.cs
@@ -0,0 +1,42 @@
+﻿using S11_E01.Entities;
+using S11_E01.Entities.Entities.Exceptions;
+
+using System.Globalization;
+
+namespace S11_E01
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string? holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new(number, holder, balance, limit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withdaw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(withdaw);
+                Console.WriteLine($"New Balance: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error: {e.Message}");
+            }
+        }
+    }
+}
