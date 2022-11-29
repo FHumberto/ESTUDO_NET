11ca1d49ded266462bdde4a8b4f83171192aad7a
Humberto Guedes <franciscohguedes@gmail.com>
2022-11-29 20:54:18 -0300

    add: resolução do exercício da seção 13: E01.

---
+++ S13-E01/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace S13_E01.Entities
+{
+    internal class Product
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalPrice()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name},{TotalPrice().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
