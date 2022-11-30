07441a5dad108eb286c13d86640a8db040d8ee5f
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2022-11-29 21:04:02 -0300

    add: Resolução do Exercício da seção 14: E01.

---
+++ S14-E01/Services/IOnlynePaymentService.cs
@@ -0,0 +1,9 @@
+﻿namespace S14_E01.Services
+{
+    internal interface IOnlynePaymentService
+    {
+        public double PaymentFee(double amount);
+
+        public double Interest(double amount, int months);
+    }
+}
