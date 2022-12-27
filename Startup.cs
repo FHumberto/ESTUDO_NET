7e44f7f851603de61c257ac9e9f4d71f7c4bcba0
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2022-12-27 03:54:23 -0300

    refact: agrupamento da CFG da aplicação na classe startup.

---
+++ Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace CNA_SalesWebMvc
+{
+    public class Startup
+    {
+        public IConfiguration ConfigRoot { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            ConfigRoot = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
