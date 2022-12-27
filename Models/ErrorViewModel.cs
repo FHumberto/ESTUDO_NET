4432c8efc3a87b223bb837cadb3bd56e0afb589a
Humberto Guedes <franciscohguedes@gmail.com>
2022-12-27 03:32:58 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CNA_SalesWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
