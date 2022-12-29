f1050c1aa1403baecf63408c774cbbe6bc3a5500
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2022-12-29 16:52:38 -0300

    add: ação de deletar o Seller.

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -17,6 +17,19 @@ namespace CNA_SalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
+        // encontra o seller com o ID informado
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj); // remove o objeto encontrado do DBSet
+            _context.SaveChanges();
+        }
+
         public void Insert(Seller obj)
         {
             _context.Add(obj); // adiciona o objeto ao banco de dados.
