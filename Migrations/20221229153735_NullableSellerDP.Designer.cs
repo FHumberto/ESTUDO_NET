e737f703cce38d68e47333cfec7e35043587a8ed
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2022-12-29 12:45:19 -0300

    add: fix nullable na prop department de seller e add na migration.

--- Migrations/20221229153735_NullableSellerDP.Designer.cs
+++ Migrations/20221229153735_NullableSellerDP.Designer.cs
@@ -4,6 +4,7 @@ using CNA_SalesWebMvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CNASalesWebMvc.Migrations
 {
     [DbContext(typeof(SalesWebMvcContext))]
-    partial class SalesWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20221229153735_NullableSellerDP")]
+    partial class NullableSellerDP
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
