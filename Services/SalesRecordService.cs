e7c0535083cb462ccba59159a548b478463f4fbd
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-01-01 06:27:43 -0300

    add: implementação da busca agrupada.

--- Services/SalesRecordService.cs
+++ Services/SalesRecordService.cs
@@ -24,13 +24,35 @@ namespace CNA_SalesWebMvc.Services
             }
             if (maxDate.HasValue)
             {
-                result = result.Where(x => x.Date >= maxDate.Value);
+                result = result.Where(x => x.Date <= maxDate.Value);
             }
 
             return await result
                 .Include(x => x.Seller) // junta a tabela seller
                 .Include(x => x.Seller.Department) // junta a tabela departamento
                 .OrderBy(x => x.Date) // ordena por data
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department?, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            // constroi um objeto resultado do tipo iquerable no banco
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller) // junta a tabela seller
+                .Include(x => x.Seller.Department) // junta a tabela departamento
+                .OrderBy(x => x.Date) // ordena por data
+                .GroupBy(x => x.Seller.Department) // muda o tipo para agrupamento
                 .ToListAsync();
         }
     }
