8788b236be9ed1db91db7d4e0e8feee1e4f6b995
Humberto Guedes <franciscohguedes@gmail.com>
2023-02-18 09:55:51 -0300

    add: CRUD com simulação de banco

---
+++ S5-CRUD/Data/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using S5_CRUD.Model;
+
+namespace S5_CRUD.Data
+{
+    public static class ProductRepository
+    {
+        public static List<Product>? Products { get; set; }
+
+        public static void Add(Product product)
+        {
+            if(Products == null)
+                Products = new List<Product>();
+            Products.Add(product);
+        }
+
+        public static void Remove(Product product)
+        {
+            if (Products != null)
+            {
+                Products.Remove(product);
+            }
+        }
+
+        public static Product GetBy(string code)
+        {
+            return Products.FirstOrDefault(p => p.Code == code);
+        }
+    }
+}
