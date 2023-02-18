88e3975804e83042115b134577ef62c3ad8bd94d
Humberto Guedes <franciscohguedes@gmail.com>
2023-02-18 08:28:26 -0300

    add: endpoints, estrutura e verbos

--- S4-API/Program.cs
+++ S4-API/Program.cs
@@ -1,18 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+using S4_API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/", () => new { Name = "Humberto Guedes", Age = 31 });
+
+app.MapGet("/AddHeader", (HttpResponse response) =>
+{
+    response.Headers.Add("Teste", "Humberto");
+    return new { Name = "Humberto Guedes", Age = 31 };
+});
+
+app.MapPost("/saveproduct", (Product product) =>
+{
+    return $"{product.Code} - {product.Name}";
+});
+
+//? FORMAS DE PASSAR DADOS
+
+// POR PARÂMETRO
+app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) =>
+{
+    return $"{dateStart} - {dateEnd}";
+});
+
+// POR ROTA
+app.MapGet("/getproduct/{code}", ([FromRoute] string code) =>
+{
+    return code;
+});
+
+// PELO HEADER
+app.MapGet("/getproductbyheader", (HttpRequest request) =>
+{
+    return request.Headers["product-code"].ToString();
+});
+
 app.Run();
+
