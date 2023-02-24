878ef8684c1fa7a6a6ca47c473d2348c47e4e502
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-02-24 03:21:27 -0300

    add: claims e rota de usuário

--- S12-PFC/Program.cs
+++ S12-PFC/Program.cs
@@ -2,13 +2,23 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 using S12_PFC.Endpoints.Categories;
+using S12_PFC.Endpoints.Employees;
 using S12_PFC.Infra.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionStr));
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false; // personaliza a validação para não precisar de alphanumericos
+    options.Password.RequireDigit = false; // não precisa de um digito
+    options.Password.RequireUppercase = false; // não precisa de upercase
+    options.Password.RequireLowercase = false; // não precisa de lowercase
+    options.Password.RequiredLength = 3;
+})
+.AddEntityFrameworkStores<AppDbContext>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -30,5 +40,6 @@ app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.H
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle).WithTags("Categories"); // ROTA EDITAR
 app.MapMethods(CategoryDelete.Template, CategoryDelete.Methods, CategoryDelete.Handle).WithTags("Categories"); // ROTA EDITAR
 
+app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle).WithTags("Employees"); // ROTA EDITAR
 
 app.Run();
