185b0e530b11a4160a57dca32fe3e9a91e1ab728
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-03-02 17:23:08 -0300

    [API/TDLS] add: modelos, conexão com o banco e primeira migração

---
+++ API/TodoListSimple/TDLS/Data/TdlsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TDLS.Models;
+
+namespace TDLS.Data;
+
+public class TdlsContext : DbContext
+{
+    public DbSet<TodoItem> TodoItems { get; set; }
+
+    public TdlsContext(DbContextOptions options) : base(options)
+    {
+    }
+}
--- API/TodoListSimple/TDLS/Program.cs
+++ API/TodoListSimple/TDLS/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+using TDLS.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TdlsContext>(options => options.UseSqlServer(connectionStr));
+
 
 var app = builder.Build();
 
