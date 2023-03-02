e46caf0e050449046400482d0362fed9a3b3720e
Humberto Guedes <franciscohguedes@gmail.com>
2023-03-02 17:23:08 -0300

    [API/TDLS] add: modelos, conexão com o banco e primeira migração

---
+++ API/TodoListSimple/TDLS/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TDLS.Models;
+
+public class TodoItem
+{
+    [Key]
+    public int Id { get; set; }
+    [Required, MaxLength(100)]
+    public string Tittle { get; set; }
+    [MaxLength(250)]
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public DateTime EditedOn { get; set; }
+}
