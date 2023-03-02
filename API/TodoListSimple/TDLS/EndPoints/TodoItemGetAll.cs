592b5d73239a106f32e243096d304a938e3223db
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-03-02 18:45:47 -0300

    [API/TDLS] add: rota get

---
+++ API/TodoListSimple/TDLS/EndPoints/TodoItemGetAll.cs
@@ -0,0 +1,16 @@
+﻿using TDLS.Data;
+
+namespace TDLS.EndPoints;
+
+public static class TodoItemGetAll
+{
+    public static string Template => "/todo"; // indica a rota do endpoint
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() }; // para determinar os 4 métodos
+    public static Delegate Handle => Action;
+
+    public static IResult Action(TdlsContext context)
+    {
+        var response = context.TodoItems.ToList();
+        return Results.Ok(response);
+    }
+}
