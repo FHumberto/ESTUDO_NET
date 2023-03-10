95ee20c4bc57d45f4fd22b3a8f45e57b3a02c16b
Humberto Guedes <franciscohguedes@hotmail.com>
2023-03-10 11:05:33 -0300

    [CRS/WebAPI] add: rota de vitrine dos produtos

--- CRS/WEBAPI/S12-PFC/Endpoints/Products/ProductResponse.cs
+++ CRS/WEBAPI/S12-PFC/Endpoints/Products/ProductResponse.cs
@@ -1,3 +1,3 @@
 ﻿namespace S12_PFC.Endpoints.Products;
 
-public record ProductResponse(string Name, string categoryName, string Description, bool HasStock, bool Active);
+public record ProductResponse(Guid Id, string Name, string categoryName, string Description, bool HasStock, decimal Price, bool Active);
