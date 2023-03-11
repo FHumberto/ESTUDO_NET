aece110f07c756ec7ecfc779acde3548a371f6e7
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-03-11 18:12:46 -0300

    [CRS/WebAPI] add: rota de pedido

---
+++ CRS/WEBAPI/S12-PFC/Endpoints/Order/OrderPost.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+using S12_PFC.Endpoints.Order;
+using S12_PFC.Infra.Data;
+
+using System.Security.Claims;
+
+namespace S12_PFC.Endpoints.Clients;
+
+public static class OrderPost
+{
+    public static string Template => "/orders";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    [Authorize(Policy = "CpfPolicy")]
+    public static async Task<IResult> Action(OrderRequest orderRequest, HttpContext http, AppDbContext context)
+    {
+        // PEGA OS DADOS DO CLIENTE AUTENTICADO (HTTP)
+        var clientId = http.User.Claims
+            .First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+        var clientName = http.User.Claims
+            .First(c => c.Type == "Name").Value;
+
+        // PERCORRE O BANCO PELOS PRODUTOS NO PEDIDO (EVITANDO O ACESSO POR LOOP FOREACH)
+        var productsFound = context.Products.Where(p => orderRequest.ProductIds.Contains(p.Id)).ToList();
+
+        //metodo de baixo desempenho
+        //foreach (var item in orderRequest.ProductIds)
+        //{
+        //    var product = context.Products.First(p => p.Id == item);
+        //    products.Add(product);
+        //}
+
+        // CONSTROI O PEDIDO
+        var order = new S12_PFC.Domain.Order.Order(clientId, clientName, productsFound, orderRequest.DeliveryAddress);
+
+        // VALIDAÇÃO
+        if (!order.IsValid)
+        {
+            return Results.ValidationProblem(order.Notifications.ConvertToProblemDetails());
+        }
+
+        await context.Orders.AddAsync(order);
+        await context.SaveChangesAsync();
+
+        return Results.Created($"/orders/{order.Id}", order.Id);
+    }
+}
---
+++ CRS/WEBAPI/S12-PFC/Endpoints/Order/OrderRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace S12_PFC.Endpoints.Order;
+
+// OS DADOS DO CLIENTE JÁ VAI ESTÁ NO TOKEN
+public record OrderRequest(List<Guid> ProductIds, string DeliveryAddress);
