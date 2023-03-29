8c8d5fe7ac086c0a7d2436d07106245666e2f868
Humberto Guedes <franciscohguedes@gmail.com>
2023-03-29 17:29:24 -0300

    [CRS/Skinet] add: modelos, pacotes e conexão com o banco de dados

---
+++ CRS/Skinet/API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public string GetProducts()
+    {
+        return "this will be a list of products";
+    }
+
+    [HttpGet]
+    public string GetProduct(int id)
+    {
+        return "this will be a product";
+    }
+}
