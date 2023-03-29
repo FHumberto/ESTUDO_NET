78134b903b20625744664e32d506f7221813b881
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-03-29 18:57:52 -0300

    [CRS/Skinet] refact: organização na arquitetura do projeto e gitignore

--- CRS/Skinet/API/Controllers/ProductsController.cs
+++ CRS/Skinet/API/Controllers/ProductsController.cs
@@ -1,26 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructue.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController : ControllerBase
+namespace API.Controllers
 {
-    [HttpGet]
-    public string GetProducts()
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
     {
-        return "this will be a list of products";
-    }
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
 
-    [HttpGet]
-    public string GetProduct(int id)
-    {
-        return "this will be a product";
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
     }
 }
