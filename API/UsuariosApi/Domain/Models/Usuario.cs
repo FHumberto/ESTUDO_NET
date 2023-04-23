5782de37c4db6f40601f169f6ef1905ef3360082
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-04-23 03:35:59 -0300

    [API/UsuariosApi] add: rotas de CRUD e validação de modelo

--- API/UsuariosApi/Domain/Models/Usuario.cs
+++ API/UsuariosApi/Domain/Models/Usuario.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System.ComponentModel.DataAnnotations;
 
 namespace UsuariosApi.Domain.Models;
 
-public class Usuario
+public class Usuario : Notifiable<Notification>
 {
     [Key]
     public int Id { get; set; }
@@ -11,4 +13,32 @@ public class Usuario
     public string? Sobrenome { get; set; }
     [Required, EmailAddress]
     public string? Email { get; set; }
+
+    public Usuario(string? nome, string? sobrenome, string? email)
+    {
+        Nome = nome;
+        Sobrenome = sobrenome;
+        Email = email;
+
+        Validar();
+    }
+
+    public void Editar(string? nome, string? sobrenome, string? email)
+    {
+        Nome = nome;
+        Sobrenome = sobrenome;
+        Email = email;
+
+        Validar();
+    }
+
+
+    private void Validar()
+    {
+        var contract = new Contract<Usuario>()
+            .IsNotNullOrEmpty(Nome, "Nome")
+            .IsGreaterOrEqualsThan(Nome, 3, "Nome")
+            .IsEmailOrEmpty(Email, "Email");
+        AddNotifications(contract);
+    }
 }
