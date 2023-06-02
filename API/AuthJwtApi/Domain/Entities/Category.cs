af33b8b91f3dc6e9edfddc71e48498a850c779de
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-06-02 07:21:42 -0300

    add: pacotes e dbcontext

---
+++ API/AuthJwtApi/Domain/Entities/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthJwtApi.Domain.Entities;
+
+public class Category : BaseEntity
+{
+    public string? Name { get; set; }
+    public ICollection<Product> Product { get; set; }
+}
--- API/AuthJwtApi/Domain/Entities/Product.cs
+++ API/AuthJwtApi/Domain/Entities/Product.cs
@@ -1,8 +1,13 @@
-﻿namespace AuthJwtApi.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthJwtApi.Domain.Entities;
 
 public class Product : BaseEntity
 {
+    [Required]
     public string? Name { get; set; }
     public float Price { get; set; }
     public string? Description { get; set; } = string.Empty;
+    public Category Category { get; set; }
 }
