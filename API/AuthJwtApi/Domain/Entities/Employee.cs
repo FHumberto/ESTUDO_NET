318597968342db80ae9a7807781f235e3d82dd26
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-06-02 06:25:40 -0300

    [Api/AuthJwtApi] add: entidades

---
+++ API/AuthJwtApi/Domain/Entities/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthJwtApi.Domain.Entities;
+
+public class Employee : BaseEntity
+{
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    public string Name { get; set; }
+    public string? Role { get; set; }
+}
