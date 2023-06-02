924bcc1718acfbda3353a5f086ffda613a8fbdc4
Humberto Guedes <franciscohguedes@hotmail.com>
2023-06-02 06:25:40 -0300

    [Api/AuthJwtApi] add: entidades

---
+++ API/AuthJwtApi/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace AuthJwtApi.Domain.Entities;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+}
---
+++ API/AuthJwtApi/Domain/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthJwtApi.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string? Name { get; set; }
+    public float Price { get; set; }
+    public string? Description { get; set; } = string.Empty;
+}
