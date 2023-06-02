9e39903770c1083e71659d5c37136cecb0d95700
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-06-02 08:01:53 -0300

    add: diretório [EntityFrameworkCore] ao [CRS]

---
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CodingWiki_Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
