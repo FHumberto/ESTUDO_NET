cc4c00a1d8d273ac831772ef2f5bf1c0f60be3f9
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-06-04 13:46:02 -0300

    [CRS/EntityFramework] add: modelos, conexão com banco e primeira migração

---
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_DataAcess/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAcess.Data;
+internal class ApplicationDbContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True;");
+
+    }
+}
---
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Model/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_Model.Models;
+public class Book
+{
+    public int BookId { get; set; }
+    public string Tittle { get; set; }
+    public string ISBN { get; set; }
+    public double Price { get; set; }
+}
