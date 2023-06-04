a6da2e4ee0ffcd3b5144c157ceb5320294ccb5e6
Humberto Guedes <franciscohguedes@hotmail.com>
2023-06-04 17:38:10 -0300

    [CRS/EntityFramework] add: nova migração de rename

--- CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Model/Models/Genre.cs
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Model/Models/Genre.cs
@@ -9,5 +9,5 @@ public class Genre
 {
     public int GenreId { get; set; }
     public string GenreName { get; set; }
-    public int Display { get; set; }
+    public int DisplayOrder { get; set; }
 }
