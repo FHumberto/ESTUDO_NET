78d752f97f7d8a08cd7e8340835a2be559630208
Humberto Guedes <franciscohguedes@hotmail.com>
2023-06-04 18:36:42 -0300

    [CRS/EntityFramework] add: data seeding

--- CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Model/Models/Book.cs
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Model/Models/Book.cs
@@ -9,7 +9,7 @@ namespace CodingWiki_Model.Models;
 public class Book
 {
     public int BookId { get; set; }
-    public string Tittle { get; set; }
+    public string Title { get; set; }
     public string ISBN { get; set; }
     public decimal Price { get; set; }
 }
