92715c19916fc939b636ce526f055ec148af194d
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-06-04 16:04:33 -0300

    [CRS/EntityFramework] add: modelo Genre e nova migração

--- CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_DataAcess/Migrations/20230604190005_AddGenresToDb.Designer.cs
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_DataAcess/Migrations/20230604190005_AddGenresToDb.Designer.cs
@@ -3,6 +3,7 @@ using CodingWiki_DataAcess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CodingWiki_DataAcess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230604190005_AddGenresToDb")]
+    partial class AddGenresToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
