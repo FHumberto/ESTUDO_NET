8378fd2807b75274ea6c8078bc0bd348ad4a3575
Humberto Guedes <franciscohguedes@hotmail.com>
2023-06-18 16:35:30 -0300

    [CRS/EntityFramework] add: classes fluentAPI

---
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Model/Models/FluentModels/Fluent_Publisher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_Model.Models;
+[Table("Publishers")]
+public class Fluent_Publisher
+{
+    [Key]
+    public int Publisher_Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public string Location { get; set; }
+    // public List<Fluent_Book> Books { get; set; }
+}
