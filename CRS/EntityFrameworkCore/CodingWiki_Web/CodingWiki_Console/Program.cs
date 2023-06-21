1bbe9ebcaabae94422a5237f9f13794ebdd0ae29
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-06-21 05:12:18 -0300

    add: metodos de query e contexto

--- CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Console/Program.cs
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Console/Program.cs
@@ -14,12 +14,70 @@ Console.WriteLine("Hello, World!");
 //    }
 //}
 
-AddBook();
-GetAllBooks();
+//AddBook();
+//GetBook();
+//GetBookById(5);
+//GetAllBooks();
+
+void DeleteBook(int id)
+{
+    try
+    {
+        using ApplicationDbContext context = new();
+        var book = context.Books.Find(1);
+        context.Books.Remove(book);
+        context.SaveChanges();
+    }
+    catch (Exception e)
+    {
+
+    }
+}
+
+
+void update()
+{
+    try
+    {
+        using ApplicationDbContext context = new();
+        var book = context.Books.Find(1);
+        Console.WriteLine($"{book.Title} - {book.ISBN}");
+        book.ISBN = "777";
+        context.SaveChanges();
+    }
+    catch (Exception e)
+    {
+
+    }
+}
+
+void GetBook()
+{
+    try
+    {
+        using ApplicationDbContext context = new();
+        var books = context.Books.ToList().Skip(0).Take(2);
+        foreach (Book? book in books)
+        {
+            Console.WriteLine($"{book.Title} - {book.ISBN}");
+        }
+    }
+    catch(Exception e)
+    {
+
+    }
+}
+
+void GetBookById(int id)
+{
+    using ApplicationDbContext context = new ();
+    var book = context.Books.Where(b => b.Publisher_Id == id).FirstOrDefault();
+    Console.WriteLine($"{book.Title} - {book.ISBN}");
+}
 
 void GetAllBooks()
 {
-    using ApplicationDbContext context = new ApplicationDbContext();
+    using ApplicationDbContext context = new();
     List<Book> books = context.Books.ToList();
     foreach (Book? book in books)
     {
