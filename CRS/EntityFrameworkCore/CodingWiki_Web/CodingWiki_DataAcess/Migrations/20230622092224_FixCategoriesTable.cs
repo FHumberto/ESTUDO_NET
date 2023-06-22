eb2b1cc8ef9f937409b9bfa1e6a450480afb9065
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-06-22 06:27:56 -0300

    [CRS/EntityFramework] add: ajustes em category e novas migrações

---
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_DataAcess/Migrations/20230622092224_FixCategoriesTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAcess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCategoriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "Categories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
