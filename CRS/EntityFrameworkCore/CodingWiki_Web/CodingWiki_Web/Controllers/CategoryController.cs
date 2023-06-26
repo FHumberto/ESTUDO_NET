6ecd3f8f9be326e68ee6a161c9ed6448726c7868
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-06-26 02:07:55 -0300

    [CRS/EF] add: view category

---
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Web/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using CodingWiki_DataAcess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers;
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        List<Category> objList = _db.Categoiries.ToList();
+        return View(objList);
+    }
+}
