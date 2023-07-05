fcf282ac3bf2faffd1b3ae6ece1ad2d1ff70d6f0
Humberto Guedes <franciscohguedes@hotmail.com>
2023-07-05 07:16:35 -0300

    [CRS/EF] add: controladores e interfaces para Author e Publisher

---
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Web/Controllers/AuthorController.cs
@@ -0,0 +1,69 @@
+﻿using CodingWiki_DataAcess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers;
+public class AuthorController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public AuthorController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        List<Author> objList = _db.Authors.ToList();
+        return View(objList);
+    }
+
+    public IActionResult Upsert(int? id)
+    {
+        Author obj = new();
+        if (id == null || id == 0)
+        {
+            // create
+            return View(obj);
+        }
+        //edit
+        obj = _db.Authors.FirstOrDefault(u => u.Author_Id == id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        return View(obj);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Upsert(Author obj)
+    {
+        if (ModelState.IsValid)
+        {
+            //create
+            await _db.Authors.AddAsync(obj);
+        }
+        else
+        {
+            //update
+            _db.Authors.Update(obj);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(obj);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        Author obj = new();
+        obj = _db.Authors.FirstOrDefault(u => u.Author_Id == id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        _db.Authors.Remove(obj);
+        await _db.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Web/Controllers/PublisherController.cs
@@ -0,0 +1,69 @@
+﻿using CodingWiki_DataAcess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers;
+public class PublisherController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public PublisherController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        List<Publisher> objList = _db.Publishers.ToList();
+        return View(objList);
+    }
+
+    public IActionResult Upsert(int? id)
+    {
+        Publisher obj = new();
+        if (id == null || id == 0)
+        {
+            // create
+            return View(obj);
+        }
+        //edit
+        obj = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        return View(obj);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Upsert(Publisher obj)
+    {
+        if (ModelState.IsValid)
+        {
+            //create
+            await _db.Publishers.AddAsync(obj);
+        }
+        else
+        {
+            //update
+            _db.Publishers.Update(obj);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(obj);
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        Publisher obj = new();
+        obj = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        _db.Publishers.Remove(obj);
+        await _db.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
