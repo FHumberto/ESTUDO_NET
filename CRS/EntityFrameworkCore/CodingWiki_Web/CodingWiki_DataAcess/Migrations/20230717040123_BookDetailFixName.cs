d4cc7daecbe960938c658f39d3d2846b458c55ac
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-07-17 01:02:27 -0300

    [CRS/EF] add: ajustes no nome da tabela bookdetail e nova migração

---
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_DataAcess/Migrations/20230717040123_BookDetailFixName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAcess.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookDetailFixName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NumberOfCharpters",
+                table: "BookDetails",
+                newName: "NumberOfChapters");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NumberOfChapters",
+                table: "BookDetails",
+                newName: "NumberOfCharpters");
+        }
+    }
+}
