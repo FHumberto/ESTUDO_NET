5c0924a7632c6850b217b0a841ac8d9357ddb3a4
Humberto Guedes <franciscohguedes@hotmail.com>
2023-07-17 01:02:27 -0300

    [CRS/EF] add: ajustes no nome da tabela bookdetail e nova migração

--- CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Model/Models/BookDetail.cs
+++ CRS/EntityFrameworkCore/CodingWiki_Web/CodingWiki_Model/Models/BookDetail.cs
@@ -2,12 +2,13 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CodingWiki_Model.Models;
+
 public class BookDetail
 {
     [Key]
     public int BookDetail_Id { get; set; }
     [Required]
-    public int NumberOfCharpters { get; set; }
+    public int NumberOfChapters { get; set; }
     public int NumberOfPages { get; set; }
     public string Weight { get; set; }
     [ForeignKey("Book")]
