529ef7dc0d32dcb1d95d79e4b44d10168a97b5c0
Humberto Guedes <franciscohguedes@hotmail.com>
2023-07-25 03:24:09 -0300

    [Api\ProductHub] add: produto, conexão com o banco e primeira migração

---
+++ Projetos/API/ProductHub/ProductHub_Domain/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductHub_Domain.Models;
+public class Product
+{
+    [Key]
+    public int Product_Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+}
