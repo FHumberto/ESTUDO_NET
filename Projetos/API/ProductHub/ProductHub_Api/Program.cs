6e97f8abf708d3a647250edaf3db4c5b16ca2c2e
Humberto Guedes <franciscohguedes@hotmail.com>
2023-07-25 00:10:08 -0300

    [Api\ProductHub] refact: remoção de arquivos desnecessários

--- Projetos/API/ProductHub/ProductHub_Api/Program.cs
+++ Projetos/API/ProductHub/ProductHub_Api/Program.cs
@@ -1,15 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
