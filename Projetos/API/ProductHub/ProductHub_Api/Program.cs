3096276eb7407471cc27590af470d5805d1c87e3
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-07-25 00:51:04 -0300

    [Api\ProductHub] add: configuação básica do swagger

--- Projetos/API/ProductHub/ProductHub_Api/Program.cs
+++ Projetos/API/ProductHub/ProductHub_Api/Program.cs
@@ -1,9 +1,31 @@
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Version = "v1",
+        Title = "Product Hub",
+        Description = "Sistema de catalogo e gerenciamento de produto simples. Para estudo de tecnicas avançadas com .net.",
+        Contact = new OpenApiContact
+        {
+            Name = "Humberto Guedes",
+            Email = "fhumberto.trab@gmail.com",
+            Url = new Uri(Environment.GetEnvironmentVariable("Contact") ?? "Arquivo de configuração não encontrado"),
+        },
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 WebApplication app = builder.Build();
 
