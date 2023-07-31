c54b60060544ad08189bdf19f37caf7556efea52
Humberto Guedes <franciscohguedes@hotmail.com>
2023-07-31 04:06:56 -0300

    [Recursos\UsersCatalog] add: modelo de DTO e rotas de CRUD

--- Recursos/API/UsersCatalog/UsersCatalog.Api/Controllers/UserController.cs
+++ Recursos/API/UsersCatalog/UsersCatalog.Api/Controllers/UserController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UsersCatalog.DataAccess.Data;
+using UsersCatalog.Models;
+using UsersCatalog.Models.DTOs;
+using UsersCatalog.Models.Models;
 
 namespace UsersCatalog.Api.Controllers;
 
@@ -19,5 +22,107 @@ public class UserController : ControllerBase
     public ActionResult GetAll()
     {
         return Ok(_db.Users.ToList());
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<UserDto> GetById(int id)
+    {
+        if (id == 0)
+        {
+            return BadRequest();
+        }
+
+        var user = _db.Users.Find(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        UserDto userDto = new UserDto();
+
+        userDto.FirstName = user.FirstName;
+        userDto.LastName = user.LastName;
+        userDto.Email = user.Email;
+
+        return Ok(userDto);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<UserDto> CreateUser([FromBody] UserDto userDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        var newUser = new User
+        {
+            FirstName = userDto.FirstName,
+            LastName = userDto.LastName,
+            Email = userDto.Email,
+        };
+
+        _db.Users.Add(newUser);
+        _db.SaveChanges();
+
+        return Ok();
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<UserDto> UpdateUser(int id, [FromBody] UserDto userDto)
+    {
+        if (userDto is null)
+        {
+            return BadRequest();
+        }
+
+        var user = _db.Users.Find(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        user.FirstName = userDto.FirstName;
+        user.LastName = userDto.LastName;
+        user.Email = userDto.Email;
+
+        _db.Update(user);
+        _db.SaveChanges();
+
+        return Ok(userDto);
+    }
+
+    [HttpDelete("{id:int}", Name = "Delete User")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<UserDto> DeleteUser (int id)
+    {
+        if(id == 0)
+        {
+            return BadRequest();
+        }
+
+        var user = _db.Users.Find(id);
+
+        if(user is null)
+        {
+            return NotFound();
+        }
+
+        _db.Remove(user);
+        _db.SaveChanges();
+
+        return Ok("Usuário removido.");
     }
 }
---
+++ Recursos/API/UsersCatalog/UsersCatalog.Models/DTOs/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersCatalog.Models.DTOs;
+
+public class UserDto
+{
+    [Required]
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    [Required, EmailAddress]
+    public string? Email { get; set; }
+}
