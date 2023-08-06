543629453263562d911cfad7daf7bd263fb48737
Humberto Guedes <franciscohguedes@hotmail.com>
2023-08-06 16:36:19 -0300

    [CRS/MagicVilla] add: padrão repositório, tipos genéricos e ajustes no mapconfig

--- CRS/MagicVilla/MagicVilla_VillaAPI/Migrations/20230802094418_FixVillaTable.cs
+++ CRS/MagicVilla/MagicVilla_VillaAPI/Migrations/20230802094418_FixVillaTable.cs
@@ -2,53 +2,52 @@
 
 #nullable disable
 
-namespace MagicVilla_VillaAPI.Migrations
+namespace MagicVilla_VillaAPI.Migrations;
+
+/// <inheritdoc />
+public partial class FixVillaTable : Migration
 {
     /// <inheritdoc />
-    public partial class FixVillaTable : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<double>(
-                name: "Rate",
-                table: "Villas",
-                type: "float",
-                nullable: false,
-                defaultValue: 0.0,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
+        migrationBuilder.AlterColumn<double>(
+            name: "Rate",
+            table: "Villas",
+            type: "float",
+            nullable: false,
+            defaultValue: 0.0,
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)",
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Name",
-                table: "Villas",
-                type: "nvarchar(max)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "Name",
+            table: "Villas",
+            type: "nvarchar(max)",
+            nullable: false,
+            defaultValue: "",
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)",
+            oldNullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "Rate",
-                table: "Villas",
-                type: "nvarchar(max)",
-                nullable: true,
-                oldClrType: typeof(double),
-                oldType: "float");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "Rate",
+            table: "Villas",
+            type: "nvarchar(max)",
+            nullable: true,
+            oldClrType: typeof(double),
+            oldType: "float");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Name",
-                table: "Villas",
-                type: "nvarchar(max)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "Name",
+            table: "Villas",
+            type: "nvarchar(max)",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)");
     }
 }
