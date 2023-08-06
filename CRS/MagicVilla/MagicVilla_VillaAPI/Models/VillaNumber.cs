c2d9c54c458760f4cb14f43b60e1a14891b8ca65
Humberto Guedes <franciscohguedes@hotmail.com>
2023-08-06 17:59:47 -0300

    [CRS/MagicVilla] add: modelos e dtos para VillaNumber

---
+++ CRS/MagicVilla/MagicVilla_VillaAPI/Models/VillaNumber.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_VillaAPI.Models;
+
+public class VillaNumber
+{
+    // desabilita a geração do Id, para ser definido pelo usuário.
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int VillaNo { get; set; }
+    public string SpecialDetails { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+}
