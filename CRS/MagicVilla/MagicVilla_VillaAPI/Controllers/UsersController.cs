84b21ca096186f32b853ad262d71821cca9f470b
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2023-08-28 03:05:07 -0300

    [CRS/MagicVilla] add: modelo, serviço e controlador de usuario

---
+++ CRS/MagicVilla/MagicVilla_VillaAPI/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MagicVilla_VillaAPI.Controllers;
+
+[Route("api/UsersAuth")]
+[ApiController]
+
+public class UsersController : ControllerBase
+{
+    private readonly IUserRepository _userRepository;
+    protected ApiResponse _response;
+
+    public UsersController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+        this._response = new();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequest)
+    {
+        var loginResponse = await _userRepository.Login(loginRequest);
+
+        if (loginResponse.User == null || string.IsNullOrEmpty(loginResponse.Token))
+        {
+            _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Username or password is incorrect");
+            return BadRequest(_response);
+        }
+
+        _response.StatusCode = HttpStatusCode.OK;
+        _response.IsSuccess = true;
+        _response.Result = loginResponse;
+        return Ok(_response);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegistrationRequestDto registrationRequest)
+    {
+        bool ifUserNameUnique = _userRepository.IsUniqueUser(registrationRequest.UserName);
+
+        if (!ifUserNameUnique)
+        {
+            _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Username already exists");
+            return BadRequest(_response);
+        }
+
+        var user = await _userRepository.Register(registrationRequest);
+
+        if (user == null)
+        {
+            _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Error while registering");
+            return BadRequest(_response);
+        }
+
+        _response.StatusCode = HttpStatusCode.OK;
+        _response.IsSuccess = true;
+
+        return Ok(_response);
+    }
+}
---
+++ CRS/MagicVilla/MagicVilla_VillaAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MagicVilla_VillaAPI.Models.Dto;
+
+public class LoginResponseDto
+{
+    public LocalUser User { get; set; }
+    public string Token { get; set; }
+}
