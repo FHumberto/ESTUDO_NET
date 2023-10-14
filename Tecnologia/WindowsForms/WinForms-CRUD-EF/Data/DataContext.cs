2043bb188cf01d61ca4084bbc16e99e06a160c76
FHumberto <franciscohguedes@hotmail.com>
2023-10-14 04:46:41 -0300

    Add: EntityFramework, Modelo, Mapeamento e Conexão com o banco

---
+++ Tecnologia/WindowsForms/WinForms-CRUD-EF/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+internal class DataContext : DbContext
+{
+    public DbSet<Produtos> Produtos { get; set; }
+
+    public DataContext() : base()
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ProdutosDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new Maps.ProdutosMap());
+    }
+}
---
+++ Tecnologia/WindowsForms/WinForms-CRUD-EF/Data/Migrations/20231014074157_ProdutosMap.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProdutosMap : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Unitario",
+                table: "Produtos",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "varchar(70)",
+                maxLength: 70,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Unitario",
+                table: "Produtos",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(70)",
+                oldMaxLength: 70);
+        }
+    }
+}
