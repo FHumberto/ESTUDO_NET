8b81812baffdac2c3bb01ec7291f7e94ab4710a6
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2024-02-18 05:32:18 -0300

    [HR.LeaveManagement] add: camada de infra, implementação de repositório genérico e específicos.

--- CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -8,9 +8,10 @@ namespace HR.LeaveManagement.Application.Contracts.Persistence;
 
 public interface IGenericRepository<T> where T : class
 {
-    Task<List<T>> GetAsync();
+    //? IReadOnlyList para ser imutável
+    Task<IReadOnlyList<T>> GetAsync();
     Task<T> GetByIdAsync(int id);
-    Task<T> CreateAsync(T entity);
-    Task<T> UpdateAsync(T entity);
-    Task<T> DeleteAsync(T entity);
+    Task CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
 }
---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/Configurations/LeaveTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Configurations;
+
+public class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
+{
+    public void Configure(EntityTypeBuilder<LeaveType> builder)
+    {
+        // exemplo de seed
+        builder.HasData(
+            new LeaveType { Id = 1, Name = "Vacation", DefaultDays = 10, DateCreated = DateTime.Now, DateModified = DateTime.Now });
+
+        // design
+        builder.Property(q => q.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
