576ae3ef65f08e13bac9ed58c30fa44232cf5026
Humberto Guedes <franciscohguedes@hotmail.com>
2024-02-18 05:32:18 -0300

    [HR.LeaveManagement] add: camada de infra, implementação de repositório genérico e específicos.

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,36 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using HR.LeaveManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // injeta o entity framework a camada de serviços
+        services.AddDbContext<HrDatabaseContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+        });
+
+        //? injeta a dependência o contrato IGeneric
+        //Repository e o Repository do GenericRepository
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        //? repositórios específicos
+        services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+        services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+        services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+
+        return services;
+    }
+}
