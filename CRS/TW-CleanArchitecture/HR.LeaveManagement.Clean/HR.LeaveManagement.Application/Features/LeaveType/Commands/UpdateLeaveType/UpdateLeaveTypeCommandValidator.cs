e13339244385c29daf1ab044a26ff8e37d6c8f2c
Humberto Guedes <franciscohguedes@hotmail.com>
2024-02-17 22:52:37 -0300

    [HR.LeaveManagement] add: validação e tratamento de exception para a entidade LeaveType

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+
+public class UpdateLeaveTypeCommandValidator : AbstractValidator<UpdateLeaveTypeCommand>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public UpdateLeaveTypeCommandValidator(ILeaveTypeRepository leaveTypeRepository)
+    {
+        // Name
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("{PorpertyName} is required")
+            .NotNull()
+            .MaximumLength(70).WithMessage("{PorpertyName} must be fewer than 70 characters");
+
+        // DefaultDays
+        RuleFor(p => p.DefaultDays)
+        .LessThan(100).WithMessage("{PropertyName} cannot exceed 100")
+        .GreaterThan(1).WithMessage("{PropertyName} cannot be less than 1");
+
+        RuleFor(q => q)
+            .MustAsync(LeaveTypeNameUnique)
+            .WithMessage("LeaveType alredy exists");
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+
+    private Task<bool> LeaveTypeNameUnique(UpdateLeaveTypeCommand command, CancellationToken token)
+    {
+        return _leaveTypeRepository.IsLeaveTypeUnique(command.Name);
+    }
+}
