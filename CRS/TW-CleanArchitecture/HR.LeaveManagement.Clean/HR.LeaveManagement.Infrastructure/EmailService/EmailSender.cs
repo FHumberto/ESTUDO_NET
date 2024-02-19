441ac948a67ab6d46b6509e12a519cd9b6ef8d30
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2024-02-19 02:21:50 -0300

    [HR.LeaveManagement] add: serviço de email na camada de infraestrutura

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Infrastructure/EmailService/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Infrastructure.EmailService;
+
+public class EmailSender : IEmailSender
+{
+    public EmailSettings _emailSettings { get; }
+
+    public EmailSender(IOptions<EmailSettings> emailSettings)
+    {
+        _emailSettings = emailSettings.Value;
+    }
+
+    public async Task<bool> SendEmail(EmailMessage email)
+    {
+        SendGridClient client = new(_emailSettings.ApiKey);
+        EmailAddress to = new(email.To);
+        EmailAddress from = new()
+        {
+            Email = _emailSettings.FormAddress,
+            Name = _emailSettings.FormName
+        };
+
+        SendGridMessage message = MailHelper.CreateSingleEmail(from, to, email.Subject, email.Body, email.Body);
+        Response response = await client.SendEmailAsync(message);
+
+        return response.IsSuccessStatusCode;
+    }
+}
---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using HR.LeaveManagement.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Infrastructure;
+
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        //? pega uma seção do arquivo de configuração
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        //? transient para criar uma nova instância a cada vez que é usado
+        services.AddTransient<IEmailSender, EmailSender>();
+
+        return services;
+    }
+}
