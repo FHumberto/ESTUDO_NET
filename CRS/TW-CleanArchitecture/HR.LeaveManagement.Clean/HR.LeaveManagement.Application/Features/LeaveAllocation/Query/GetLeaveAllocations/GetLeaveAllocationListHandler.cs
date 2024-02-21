6533e90e840cebe11dc2969102992ec06ccd0477
Humberto Guedes <franciscohguedes@hotmail.com>
2024-02-21 08:26:56 -0300

    [HR.LeaveManagement] add: as query da feature LeaveAllocation e seus respectivos mapeamentos

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveAllocation/Query/GetLeaveAllocations/GetLeaveAllocationListHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Query.GetLeaveAllocations;
+
+public class GetLeaveAllocationListHandler : IRequestHandler<GetLeaveAllocationListQuery, List<LeaveAllocationsDto>>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveAllocationRepository _leaveAllocation;
+
+    public GetLeaveAllocationListHandler(IMapper mapper, ILeaveAllocationRepository leaveAllocation)
+    {
+        _mapper = mapper;
+        _leaveAllocation = leaveAllocation;
+    }
+
+    public async Task<List<LeaveAllocationsDto>> Handle(GetLeaveAllocationListQuery request, CancellationToken cancellationToken)
+    {
+        //! TODO
+        //- records de um usuário específico
+        //- get allocations por employee
+        IReadOnlyList<Domain.LeaveAllocation> leaveAllocations = await _leaveAllocation.GetAsync();
+
+        List<LeaveAllocationsDto> data = _mapper.Map<List<LeaveAllocationsDto>>(leaveAllocations);
+
+        return data;
+    }
+}
---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Query.GetLeaveAllocations;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.MappingProfiles;
+
+public class LeaveAllocationProfile : Profile
+{
+    public LeaveAllocationProfile()
+    {
+        CreateMap<LeaveAllocationsDto, LeaveAllocation>().ReverseMap();
+        CreateMap<LeaveAllocationsDto, LeaveAllocation>();
+        //CreateMap<CreateLeaveAllocationCommand, LeaveAllocation>();
+        //CreateMap<UpdateLeaveAllocationCommand, LeaveAllocation>();
+    }
+}
