56e6e55f84c696288226d9dcc8b2ccec23e45b27
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2024-02-21 08:26:56 -0300

    [HR.LeaveManagement] add: as query da feature LeaveAllocation e seus respectivos mapeamentos

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveAllocation/Query/GetLeaveAllocationDetails/GetLeaveAllocationDetailsHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Query.GetLeaveAllocations;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Query.GetLeaveAllocationDetails;
+
+public class GetLeaveAllocationDetailsHandler : IRequestHandler<GetLeaveAllocationDetailsQuery, LeaveAllocationDetailsDto>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+    public GetLeaveAllocationDetailsHandler(IMapper mapper, ILeaveAllocationRepository leaveAllocationRepository)
+    {
+        _mapper = mapper;
+        _leaveAllocationRepository = leaveAllocationRepository;
+    }
+
+    public async Task<LeaveAllocationDetailsDto> Handle(GetLeaveAllocationDetailsQuery request, CancellationToken cancellationToken)
+    {
+        //? query
+        var leaveAllocationDetails = await _leaveAllocationRepository.GetByIdAsync(request.Id);
+
+        //? validação
+        if (leaveAllocationDetails is null)
+        {
+            throw new NotFoundException(nameof(leaveAllocationDetails), request.Id);
+        }
+
+        //? conversão
+        LeaveAllocationDetailsDto data = _mapper.Map<LeaveAllocationDetailsDto>(leaveAllocationDetails);
+
+        return data;
+    }
+}
