dded839918500c1c1a931d96aff2b27c1b3ac98c
Humberto Guedes <franciscohguedes@hotmail.com>
2024-02-22 13:26:45 -0300

    [HR.LeaveManagement] add: a feature leaveRequest (controaldor, comandos e querys)

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CancelLeaveRequest/CancelLeaveRequestCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Models.Email;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CancelLeaveRequest;
+
+public class CancelLeaveRequestCommandHandler : IRequestHandler<CancelLeaveRequestCommand, Unit>
+{
+    private readonly ILeaveRequestRepository _leaveRequestRepository;
+    private readonly IEmailSender _emailSender;
+
+    public CancelLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IEmailSender emailSender)
+    {
+        _leaveRequestRepository = leaveRequestRepository;
+        _emailSender = emailSender;
+    }
+
+    public async Task<Unit> Handle(CancelLeaveRequestCommand request, CancellationToken cancellationToken)
+    {
+        //? validação
+        Domain.LeaveRequest leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+
+        if (leaveRequest is null)
+            throw new NotFoundException(nameof(leaveRequest), request.Id);
+
+        //? query
+        leaveRequest.Cancelled = true;
+
+        //? execução
+        var email = new EmailMessage
+        {
+            To = string.Empty, /* Get email from employee record */
+            Body = $"Your leave request for {leaveRequest.StartDate:D} to {leaveRequest.EndDate:D} " +
+                    $"has been cancelled successfully.",
+            Subject = "Leave Request Cancelled"
+        };
+
+        await _emailSender.SendEmail(email);
+        
+        return Unit.Value;
+    }
+}
---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveRequest/Querys/GetLeaveRequestDetail/GetLeaveRequestDetailQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Querys.GetLeaveRequestDetail;
+
+public class GetLeaveRequestDetailQuery : IRequest<LeaveRequestDetailsDto>
+{
+    public int Id { get; set; }
+}
