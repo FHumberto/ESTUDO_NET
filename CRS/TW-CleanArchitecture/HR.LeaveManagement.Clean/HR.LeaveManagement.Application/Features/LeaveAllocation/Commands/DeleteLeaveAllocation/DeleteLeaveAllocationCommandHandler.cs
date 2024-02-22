80d92f7f271d39b9428322d52995dce885ee1846
Humberto Guedes <franciscohguedes@hotmail.com>
2024-02-22 07:42:18 -0300

    [HR.LeaveManagement] add: a feature leaveAllocations (controaldor, comandos e querys)

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/DeleteLeaveAllocation/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.DeleteLeaveAllocation;
+
+public class DeleteLeaveAllocationCommandHandler : IRequestHandler<DeleteLeaveAllocationCommand, Unit>
+{
+    private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+    public DeleteLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepository)
+    {
+        _leaveAllocationRepository = leaveAllocationRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+    {
+        //? query
+        var leaveAllocationDelete = await _leaveAllocationRepository.GetByIdAsync(request.Id);
+
+        //? validação
+        if (leaveAllocationDelete is null)
+            throw new NotFoundException(nameof(leaveAllocationDelete), request.Id);
+
+        //? execução
+        await _leaveAllocationRepository.DeleteAsync(leaveAllocationDelete);
+
+        //? retorna o id
+        return Unit.Value;
+    }
+}
---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/UpdateLeaveAllocation/UpdateLeaveAllocationCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.UpdateLeaveAllocation;
+
+public class UpdateLeaveAllocationCommandHandler : IRequestHandler<UpdateLeaveAllocationCommand, Unit>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public UpdateLeaveAllocationCommandHandler(IMapper mapper, ILeaveAllocationRepository leaveAllocationRepository, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _leaveAllocationRepository = leaveAllocationRepository;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+
+    public async Task<Unit> Handle(UpdateLeaveAllocationCommand request, CancellationToken cancellationToken)
+    {
+        //? validação
+        var validator = new UpdateLeaveAllocationCommandValidator(
+            _leaveTypeRepository,
+            _leaveAllocationRepository);
+
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (validationResult.Errors.Any())
+            throw new BadRequestException("Invalid Leave Allocation", validationResult);
+
+        //? query
+        var leaveAllocation = await _leaveAllocationRepository.GetByIdAsync(request.Id); //! [1]
+
+        if (leaveAllocation is null)
+            throw new NotFoundException(nameof(LeaveAllocation), request.Id);
+
+        //? conversão
+        _mapper.Map(request, leaveAllocation); //! [2] maneira mais segura para fazer o update com mapper
+
+        //? execução
+        await _leaveAllocationRepository.UpdateAsync(leaveAllocation);
+
+        return Unit.Value;
+    }
+}
