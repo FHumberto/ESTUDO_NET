3239a5f61a51325bb31b2e5bfbb1b4dd4172bcb4
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2024-02-23 07:56:06 -0300

    [HR.LeaveManagement] test: adiciona teste unitário para a query da feature leaveType

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,40 @@
+﻿
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks;
+
+public class MockLeaveTypeRepository
+{
+    public static Mock<ILeaveTypeRepository> GetMockLeaveTypeRepository()
+    {
+        List<LeaveType> leaveTypes = new ()
+        {
+            new() { Id = 1, DefaultDays = 10, Name = "Test Vacation" },
+            new() { Id = 2, DefaultDays = 15, Name = "Test Sick" },
+            new() { Id = 3, DefaultDays = 15, Name = "Test Maternity" }
+        };
+
+        //? cria uma cópia do método
+        Mock<ILeaveTypeRepository> mockRepo = new ();
+
+        //? quando usa o get
+        mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(leaveTypes);
+
+        //? quando usa o create
+        mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+            .Returns((LeaveType leaveType) =>
+            {
+                leaveTypes.Add(leaveType); // adiciona a lista do leavetypes 4
+                return Task.CompletedTask;
+            });
+
+        return mockRepo;
+    }
+}
