d7220450d3e3704aba9dc9723f712c619baea662
Humberto Guedes <franciscohguedes@hotmail.com>
2024-02-25 13:08:56 -0300

    [HR.LeaveManagement] add: página index do leaveTypes, serviços e client http

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.BlazorUI/Services/LeaveTypeService.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+
+namespace HR.LeaveManagement.BlazorUI.Services;
+
+public class LeaveTypeService : BaseHttpService, ILeaveTypeService
+{
+    private readonly IMapper _mapper;
+
+    public LeaveTypeService(IClient client, IMapper mapper) : base(client)
+    {
+        _mapper = mapper;
+    }
+
+    public async Task<Response<Guid>> CreateLeaveType(LeaveTypeVM leaveType)
+    {
+        try
+        {
+            CreateLeaveTypeCommand leaveTypeCommand = _mapper.Map<CreateLeaveTypeCommand>(leaveType);
+            await _client.LeaveTypesPOSTAsync(leaveTypeCommand);
+
+            //? necessário adicionar porque a resposta é void
+            return new Response<Guid>() { Success = true };
+        }
+        catch (ApiException ex)
+        {
+            return ConvertApiExceptions<Guid>(ex);
+        }
+    }
+
+    public async Task<Response<Guid>> DeleteLeaveType(int id)
+    {
+        try
+        {
+            await _client.LeaveTypesDELETEAsync(id);
+
+            //? necessário adicionar porque a resposta é void
+            return new Response<Guid>() { Success = true };
+        }
+        catch (ApiException ex)
+        {
+            return ConvertApiExceptions<Guid>(ex);
+        }
+    }
+
+    public async Task<LeaveTypeVM> GetLeaveType(int id)
+    {
+        LeaveTypeDetailsDto leaveTypeDetail = await _client.LeaveTypesGETAsync(id);
+        return _mapper.Map<LeaveTypeVM>(leaveTypeDetail);
+    } 
+
+    public async Task<List<LeaveTypeVM>> GetLeaveTypes()
+    {
+        ICollection<LeaveTypeDto> leaveTypes = await _client.LeaveTypesAllAsync();
+        return _mapper.Map<List<LeaveTypeVM>>(leaveTypes);
+    }
+
+    public async Task<Response<Guid>> UpdateLeaveType(int id, LeaveTypeVM leaveType)
+    {
+        try
+        {
+            UpdateLeaveTypeCommand leaveTypeCommand = _mapper.Map<UpdateLeaveTypeCommand>(leaveType);
+            await _client.LeaveTypesPUTAsync(id.ToString(), leaveTypeCommand);
+
+            //? necessário adicionar porque a resposta é void
+            return new Response<Guid>(){ Success = true };
+        }
+        catch (ApiException ex)
+        {
+            return ConvertApiExceptions<Guid>(ex);
+        }
+    }
+}
