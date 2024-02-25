fbcc84fb510bc045fdab28b9e494fb614c8dd462
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2024-02-25 17:29:21 -0300

    [HR.LeaveManagement] add: camada identity, e implementação inícial de sistema de segurança

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Identity/Services/AuthService.cs
@@ -0,0 +1,127 @@
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Models.Identity;
+using HR.LeaveManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Identity.Services;
+
+public class AuthService : IAuthService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly JwtSettings _jwtSettings;
+
+    //! usando o options pattern
+    public AuthService
+        (UserManager<ApplicationUser> userManager,
+        IOptions<JwtSettings> jwtSettings,
+        SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _jwtSettings = jwtSettings.Value;
+        _signInManager = signInManager;
+    }
+
+    public async Task<AuthResponse> Login(AuthRequest request)
+    {
+        //? validação
+        var user = await _userManager.FindByEmailAsync(request.Email);
+
+        if (user is null)
+        {
+            throw new NotFoundException($"User with {request.Email} not found.", request.Email);
+        }
+
+        SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+        if (!result.Succeeded)
+        {
+            throw new BadRequestException($"Credentials for '{request.Email} aren't valid'.");
+        }
+
+        JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+
+        var response = new AuthResponse
+        {
+            Id = int.Parse(user.Id),
+            Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+            Email = user.Email,
+            UserName = user.UserName
+        };
+
+
+        return response;
+    }
+
+    public async Task<RegistrationResponse> Register(RegistrationRequest request)
+    {
+        ApplicationUser user = new()
+        {
+            Email = request.Email,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            UserName = request.UserName,
+            EmailConfirmed = true
+        };
+
+        IdentityResult result = await _userManager.CreateAsync(user, request.Password);
+
+        if (result.Succeeded)
+        {
+            //? só permite o registro de employees (regra de negócio)
+            await _userManager.AddToRoleAsync(user, "Employee");
+            return new RegistrationResponse() { UserId = user.Id };
+        }
+        else
+        {
+            StringBuilder str = new();
+            foreach (IdentityError err in result.Errors)
+            {
+                str.AppendFormat("•{0}\n", err.Description);
+            }
+
+            throw new BadRequestException($"{str}");
+        }
+    }
+
+    private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
+    {
+        IList<Claim> userClaims = await _userManager.GetClaimsAsync(user);
+        IList<string> roles = await _userManager.GetRolesAsync(user);
+
+        List<Claim> roleClaims = roles.Select(q => new Claim(ClaimTypes.Role, q)).ToList();
+
+        //? agrupamento
+        IEnumerable<Claim> claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.FirstName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim("uid", user.Id)
+        }
+        .Union(userClaims)
+        .Union(roleClaims);
+
+        SymmetricSecurityKey symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+        SigningCredentials signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+        JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(_jwtSettings.DurationMinutes), // ajustar o utc para o caso em questão
+            signingCredentials: signingCredentials);
+
+        return jwtSecurityToken;
+    }
+}
