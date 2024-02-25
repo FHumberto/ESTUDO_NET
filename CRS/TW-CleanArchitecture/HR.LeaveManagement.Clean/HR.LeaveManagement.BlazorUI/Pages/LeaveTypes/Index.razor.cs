af519d0e06ce0212becc192a58e5e525f12291b0
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2024-02-25 13:08:56 -0300

    [HR.LeaveManagement] add: página index do leaveTypes, serviços e client http

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.BlazorUI/Pages/LeaveTypes/Index.razor.cs
@@ -0,0 +1,59 @@
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
+using Microsoft.AspNetCore.Components;
+
+namespace HR.LeaveManagement.BlazorUI.Pages.LeaveTypes
+{
+    public partial class Index
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public ILeaveTypeService LeaveTypeService { get; set; }
+
+        public List<LeaveTypeVM> LeaveTypes { get; private set; }
+        public string Message { get; set; } = string.Empty;
+
+        protected void CreateLeaveType()
+        {
+            NavigationManager.NavigateTo("/leavetypes/create/");
+        }
+
+        protected void AllocateLeaveType(int id)
+        {
+            // Use Leave Allocation Service here
+        }
+
+        protected void EditLeaveType(int id)
+        {
+            NavigationManager.NavigateTo($"/leavetypes/edit/{id}");
+        }
+
+        protected void DetailsLeaveType(int id)
+        {
+            NavigationManager.NavigateTo($"/leavetypes/details/{id}");
+        }
+
+        protected async Task DeleteLeaveType(int id)
+        {
+            Services.Base.Response<Guid> response = await LeaveTypeService.DeleteLeaveType(id);
+
+            if (response.Success)
+            {
+                //? re-renderiza o componente
+                StateHasChanged();
+            }
+            else
+            {
+                Message = response.Message;
+            }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            //? fazendo o overwrite, para preencher a lista na tabela
+            LeaveTypes = await LeaveTypeService.GetLeaveTypes();
+        }
+    }
+}
