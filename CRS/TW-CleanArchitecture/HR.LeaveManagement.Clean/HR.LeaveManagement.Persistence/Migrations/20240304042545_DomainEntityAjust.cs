50f86cc3e8d14e3fffb3370f3adeb86f188b5c22
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2024-03-04 02:14:03 -0300

    [HR.LeaveManagement] fix: ajusta as colunas da tabela de leaverequest e adiciona na migration

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/Migrations/20240304042545_DomainEntityAjust.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.LeaveManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DomainEntityAjust : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "LeaveRequests",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "LeaveRequests",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateTime(2024, 3, 4, 1, 25, 45, 496, DateTimeKind.Local).AddTicks(8220), new DateTime(2024, 3, 4, 1, 25, 45, 496, DateTimeKind.Local).AddTicks(8236) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "LeaveRequests");
+
+            migrationBuilder.UpdateData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateTime(2024, 2, 20, 7, 8, 6, 619, DateTimeKind.Local).AddTicks(3744), new DateTime(2024, 2, 20, 7, 8, 6, 619, DateTimeKind.Local).AddTicks(3759) });
+        }
+    }
+}
