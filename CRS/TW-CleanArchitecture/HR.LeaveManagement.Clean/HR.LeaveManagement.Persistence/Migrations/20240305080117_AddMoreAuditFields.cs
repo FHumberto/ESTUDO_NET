ca9a7a4b1e0e475e8f75c67c011f4912eb00dbb7
Humberto Guedes <franciscohguedes@hotmail.com>
2024-03-05 05:11:22 -0300

    [HR.LeaveManagement] add: auditoria e migração

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/Migrations/20240305080117_AddMoreAuditFields.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.LeaveManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreAuditFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Created",
+                table: "LeaveTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "LeaveTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Created",
+                table: "LeaveRequests",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "LeaveRequests",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Created",
+                table: "LeaveAllocations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "LeaveAllocations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "DateCreated", "DateModified", "ModifiedBy" },
+                values: new object[] { null, new DateTime(2024, 3, 5, 5, 1, 16, 683, DateTimeKind.Local).AddTicks(3960), new DateTime(2024, 3, 5, 5, 1, 16, 683, DateTimeKind.Local).AddTicks(3973), null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "LeaveTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "LeaveTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "LeaveAllocations");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "LeaveAllocations");
+
+            migrationBuilder.UpdateData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateTime(2024, 3, 4, 1, 25, 45, 496, DateTimeKind.Local).AddTicks(8220), new DateTime(2024, 3, 4, 1, 25, 45, 496, DateTimeKind.Local).AddTicks(8236) });
+        }
+    }
+}
---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/Migrations/20240305081047_FixAuditFields.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.LeaveManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAuditFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "LeaveTypes",
+                newName: "CreatedBy");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "LeaveRequests",
+                newName: "CreatedBy");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "LeaveAllocations",
+                newName: "CreatedBy");
+
+            migrationBuilder.UpdateData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateTime(2024, 3, 5, 5, 10, 46, 428, DateTimeKind.Local).AddTicks(7781), new DateTime(2024, 3, 5, 5, 10, 46, 428, DateTimeKind.Local).AddTicks(7795) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedBy",
+                table: "LeaveTypes",
+                newName: "Created");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedBy",
+                table: "LeaveRequests",
+                newName: "Created");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedBy",
+                table: "LeaveAllocations",
+                newName: "Created");
+
+            migrationBuilder.UpdateData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateTime(2024, 3, 5, 5, 1, 16, 683, DateTimeKind.Local).AddTicks(3960), new DateTime(2024, 3, 5, 5, 1, 16, 683, DateTimeKind.Local).AddTicks(3973) });
+        }
+    }
+}
