17d3943b85a3a5d0480b8062cce6643f5f5bdc06
Humberto Guedes <franciscohguedes@hotmail.com>
2024-03-05 05:20:16 -0300

    [HR.LeaveManagement] add: toast e create leaveTypes

---
+++ CRS/TW-CleanArchitecture/HR.LeaveManagement.Clean/HR.LeaveManagement.BlazorUI/Pages/LeaveTypes/Create.razor.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using HR.LeaveManagement.BlazorUI;
+using HR.LeaveManagement.BlazorUI.Shared;
+using System.Net;
+using System.Numerics;
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
+using Blazored.Toast.Services;
+
+namespace HR.LeaveManagement.BlazorUI.Pages.LeaveTypes
+{
+    public partial class Create
+    {
+        [Inject]
+        NavigationManager _navManager { get; set; }
+        [Inject]
+        ILeaveTypeService _client { get; set; }
+        [Inject]
+        IToastService toastService { get; set; }
+        public string Message { get; private set; }
+
+        LeaveTypeVM leaveType = new LeaveTypeVM();
+
+        async Task CreateLeaveType()
+        {
+            var response = await _client.CreateLeaveType(leaveType);
+            if(response.Success)
+            {
+                toastService.ShowSuccess("Leave Type created Successfully");
+                toastService.ShowToast(ToastLevel.Info, "Test");
+                _navManager.NavigateTo("/leavetypes/");
+            }
+            Message = response.Message;
+        }
+    }
+}
