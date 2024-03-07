b9853565646fe9aa757320dd1a7676dade7a6e7f
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2024-03-07 09:01:49 -0300

    Adiciona o projeto [Benchmark] ao diretório de recursos

---
+++ Recursos/Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmark;
+
+//? lembrar de rodar a aplicação em modo release
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //? o método abaixo chama a classe de teste e os anotations mapeados
+        BenchmarkRunner.Run<ClasseTeste>();
+    }
+}
