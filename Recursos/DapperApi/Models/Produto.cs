245f2fa71b7379607b926735f1ff3a80475e529d
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2024-11-20 13:24:45 -0300

    [Recursos/DapperApi] Add: CRUD usando Repository Pattern

---
+++ Recursos/DapperApi/Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperApi.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int Quantidade { get; set; }
+}
