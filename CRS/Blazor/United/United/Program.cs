016b9c5d0c721c2482bbda83447086dc3a4140a8
Humberto Guedes <franciscohguedes@gmail.com>
2025-01-24 09:11:49 -0300

    [CRS/Blazor] add: arquivos de projeto blazorMix

---
+++ CRS/Blazor/United/United/Program.cs
@@ -0,0 +1,36 @@
+using United.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//redireciona para a pagina por status code
+app.UseStatusCodePagesWithRedirects("/StatusCode/{0}");
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(United.Client._Imports).Assembly);
+
+app.Run();
