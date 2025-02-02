a86ee9821975990b2c103ca6177f95d148c3198a
Humberto Guedes <franciscohguedes@gmail.com>
2025-02-02 20:53:43 -0300

    [Padroes/T-Tier] Add: contrato genérico e repositório genérico

---
+++ Padroes/T-Tier/src/T-Tier.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T_Tier.DAL.Context;
+using T_Tier.DAL.Contracts;
+using T_Tier.DAL.Entities;
+
+namespace T_Tier.DAL.Repositories;
+
+public class GenericRepository<T>(AppDbContext context) : IGenericRepository<T> where T : BaseEntity
+{
+    protected readonly AppDbContext _context = context;
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        return await _context.Set<T>()
+                             .AsNoTracking()
+                             .ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>()
+                             .AsNoTracking()
+                             .FirstOrDefaultAsync(q => q.Id == id);
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _context.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+}
