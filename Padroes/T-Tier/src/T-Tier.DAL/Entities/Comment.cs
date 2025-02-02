e8ac41e2ab3d72abcb7dde061ad8532ed6072ebe
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-02-02 11:04:34 -0300

    [Padroes/T-Tier] Add: cria os modelos do banco de dados e propriedades de navegação entre eles

---
+++ Padroes/T-Tier/src/T-Tier.DAL/Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace T_Tier.DAL.Entities;
+
+public class Comment : BaseEntity
+{
+    public int UserId { get; set; }
+    public int PostId { get; set; }
+    public required string Body { get; set; }
+
+    //? prop de navegação para o usuário que fez o comentário
+    public User? User { get; set; }
+
+    //? prop de navegação para o post associado ao comentário
+    public Post? Post { get; set; }
+}
---
+++ Padroes/T-Tier/src/T-Tier.DAL/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace T_Tier.DAL.Entities;
+
+public class User : BaseEntity
+{
+    public required string FirstName { get; set; }
+    public string? LastName { get; set; }
+    public required string Email { get; set; }
+    public required byte[] PasswordHash { get; set; }
+
+    //? prop de navegacao para posts criados pelo usuario
+    public ICollection<Post>? Posts { get; set; }
+
+    //? prop de navegacao para comentarios feitos pelo usuario
+    public ICollection<Comment>? Comments { get; set; }
+}
