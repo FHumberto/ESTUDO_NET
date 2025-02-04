8a09feea43b7f182e8ff5a5de6f1457db635869c
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-02-04 07:49:46 -0300

    [Padroes/T-Tier] Add: instala o fluent validator e adiciona os validadores das entidades do sistema

---
+++ Padroes/T-Tier/src/T-Tier.BLL/Validators/CommentValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace T_Tier.BLL.Validators;
+
+public class CommentValidator : AbstractValidator<DAL.Entities.Comment>
+{
+    public CommentValidator()
+    {
+        RuleFor(c => c.UserId)
+            .NotNull().WithMessage("O campo de {propertyname} é obrigatório")
+            .NotEmpty().WithMessage("O campo de {propertyname} precisa ser fornecido");
+
+        RuleFor(c => c.PostId)
+            .NotEmpty().WithMessage("O campo de {propertyname} precisa ser fornecido")
+            .NotNull().WithMessage("O campo de {propertyname} é obrigatório");
+
+        RuleFor(c => c.Body)
+            .NotNull().WithMessage("O campo de {propertyname} é obrigatório")
+            .NotEmpty().WithMessage("O campo de {propertyname} precisa ser fornecido")
+            .MaximumLength(255).WithMessage("O campo de {propertyname} deve ter no máximo 255 caracteres");
+    }
+}
---
+++ Padroes/T-Tier/src/T-Tier.BLL/Validators/TagValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace T_Tier.BLL.Validators;
+
+public class TagValidator : AbstractValidator<DAL.Entities.Tag>
+{
+    public TagValidator()
+    {
+        RuleFor(t => t.Name)
+            .NotNull().WithMessage("O campo de {propertyname} é obrigatório")
+            .NotEmpty().WithMessage("O campo de {propertyname} precisa ser fornecido")
+            .Length(1, 50).WithMessage("O campo de {propertyname} deve ter entre 1 e 50 caracteres");
+
+        // TODO: adicionar busca pra garantir que é úncio
+    }
+}
