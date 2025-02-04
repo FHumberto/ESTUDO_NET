58e1426b4ad91439823664fa7cf7920503f2a074
Humberto Guedes <franciscohguedes@gmail.com>
2025-02-04 07:49:46 -0300

    [Padroes/T-Tier] Add: instala o fluent validator e adiciona os validadores das entidades do sistema

---
+++ Padroes/T-Tier/src/T-Tier.BLL/Validators/PostValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace T_Tier.BLL.Validators;
+
+public class PostValidator : AbstractValidator<DAL.Entities.Post>
+{
+    public PostValidator()
+    {
+        RuleFor(p => p.Tittle)
+            .NotNull().WithMessage("O campo de {propertyname} é obrigatório")
+            .NotEmpty().WithMessage("O campo de {propertyname} precisa ser fornecido")
+            .MaximumLength(50).WithMessage("O campo de {propertyname} deve ter no máximo 50 caracteres");
+
+        RuleFor(p => p.Body)
+            .NotNull().WithMessage("O campo de {propertyname} é obrigatório")
+            .NotEmpty().WithMessage("O campo de {propertyname} precisa ser fornecido")
+            .MaximumLength(255).WithMessage("O campo de {propertyname} deve ter no máximo 255 caracteres");
+    }
+}
