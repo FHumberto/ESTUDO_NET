429be4a3df7105c25c5d1777cf648fa3c277bea4
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-02-09 02:42:58 -0300

    [Padroes/T-Tier] Add: adiciona Controller, Service, Profile e Dto da rota Get de Posts

---
+++ Padroes/T-Tier/src/T-Tier.API/Controllers/PostsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using T_Tier.BLL.DTOs.Posts;
+using T_Tier.BLL.Services;
+using T_Tier.BLL.Wrappers;
+
+namespace T_Tier.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PostsController(PostService postService) : ControllerBase
+{
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetPost(int id)
+    {
+        Response<QueryPostDto?> query = await postService.GetByIdAsync(id);
+    
+        return query.Type switch
+        {
+            ResponseTypeEnum.Success => Ok(query.Result),
+            ResponseTypeEnum.NotFound => NotFound(),
+            _ => BadRequest()
+        };
+    }
+}
---
+++ Padroes/T-Tier/src/T-Tier.DAL/Contracts/IPostRepository.cs
@@ -0,0 +1,7 @@
+﻿using T_Tier.DAL.Entities;
+
+namespace T_Tier.DAL.Contracts;
+
+public interface IPostRepository : IRepository<Post>
+{
+}
