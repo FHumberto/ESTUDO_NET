4acff142489354a9d26cfced1f73f917dfbd4a9f
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-02-09 19:13:49 -0300

    [Padroes/T-Tier] Add: tratamento de exception global

---
+++ Padroes/T-Tier/src/T-Tier.API/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace T_Tier.API.Middlewares;
+
+public class GlobalExceptionMiddleware : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync
+        (HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        ProblemDetails problemDetails = new()
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Internal Server Error",
+            Detail = exception.Message
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+        httpContext.Response.ContentType = "application/problem+json";
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
