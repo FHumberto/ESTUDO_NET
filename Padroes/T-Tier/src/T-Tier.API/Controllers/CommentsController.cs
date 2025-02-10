ffe9f31b7a0bc06cff60f3b8e10f6c684d49884f
Humberto Guedes <franciscohguedes@gmail.com>
2025-02-10 01:25:05 -0300

    [Padroes/T-Tier] WIP: funcionalidade de comentários

--- Padroes/T-Tier/src/T-Tier.API/Controllers/CommentsController.cs
+++ Padroes/T-Tier/src/T-Tier.API/Controllers/CommentsController.cs
@@ -1,14 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using T_Tier.BLL.DTOs.Comments;
 using T_Tier.BLL.Services;
+using T_Tier.BLL.Wrappers;
 
 namespace T_Tier.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CommentsController(CommentsService commentsService) : ControllerBase
+public class CommentsController(CommentService commentService) : ControllerBase
 {
-    public IActionResult Index()
-    {
-        return Ok();
-    }
+     [HttpGet]
+     [ProducesResponseType(StatusCodes.Status200OK)]
+     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+     [ProducesResponseType(StatusCodes.Status404NotFound)]
+     [SwaggerOperation(Summary = "Obter todos os Comentários",
+         Description = "Retorna uma lista com todos os Comentários cadastrados.")]
+     public async Task<IActionResult> GetAllPosts()
+     {
+         Response<IReadOnlyList<QueryCommentDto>> query = await commentService.GetAllCommentAsync();
+
+         return query.Type switch
+         {
+             ResponseTypeEnum.Success => Ok(query.Result),
+             ResponseTypeEnum.NotFound => NotFound(),
+             _ => BadRequest()
+         };
+     }
 }
---
+++ Padroes/T-Tier/src/T-Tier.BLL/Services/CommentService.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using FluentValidation.Results;
+using T_Tier.BLL.DTOs.Comments;
+using T_Tier.BLL.Validators;
+using T_Tier.BLL.Wrappers;
+using T_Tier.DAL.Contracts;
+using T_Tier.DAL.Entities;
+using static T_Tier.BLL.Wrappers.ResponseTypeEnum;
+
+namespace T_Tier.BLL.Services;
+
+public class CommentService(ICommentRepository commentRepository, IMapper mapper)
+{
+    public async Task<Response<IReadOnlyList<QueryCommentDto>>> GetAllCommentAsync()
+    {
+        IReadOnlyList<Comment> comments = await commentRepository.GetAllAsync();
+        IReadOnlyList<QueryCommentDto> response = mapper.Map<IReadOnlyList<QueryCommentDto>>(comments);
+    
+        return response == null || response.Count == 0
+            ? new Response<IReadOnlyList<QueryCommentDto>>(new List<QueryCommentDto>(), NotFound)
+            : new Response<IReadOnlyList<QueryCommentDto>>(response, Success);
+    }
+    
+    public async Task<Response<QueryCommentDto?>> GetCommentByIdAsync(int id)
+    {
+        Comment? comment = await commentRepository.GetByIdAsync(id);
+        QueryCommentDto? response = mapper.Map<QueryCommentDto>(comment);
+    
+        return response == null
+            ? new Response<QueryCommentDto?>(null, NotFound)
+            : new Response<QueryCommentDto?>(response, Success);
+    }
+
+    public async Task<Response<int>> CreateCommentAsync(CommandCommentDto request)
+    {
+        CommentValidator validationRules = new();
+        ValidationResult? validationResult = await validationRules.ValidateAsync(request);
+    
+        if (!validationResult.IsValid)
+        {
+            return new Response<int>(0, InvalidInput, validationResult.Errors.Select(e => e.ErrorMessage).ToList());
+        }
+    
+        Comment commentToCreate = mapper.Map<Comment>(request);
+        int createdCommentId = await commentRepository.CreateAsync(commentToCreate);
+    
+        return new Response<int>(createdCommentId, Success);
+    }
+
+    public async Task<Response<bool>> UpdateCommentAsync(CommandCommentDto request, int id)
+    {
+        Comment? commentToUpdate = await commentRepository.GetByIdAsync(id);
+    
+        if (commentToUpdate == null)
+        {
+            return new Response<bool>(false, NotFound);
+        }
+    
+        CommentValidator validationRules = new();
+        ValidationResult? validationResult = await validationRules.ValidateAsync(request);
+    
+        if (!validationResult.IsValid)
+        {
+            return new Response<bool>(false, InvalidInput, validationResult.Errors.Select(e => e.ErrorMessage).ToList());
+        }
+    
+        mapper.Map(request, commentToUpdate);
+    
+        await commentRepository.UpdateAsync(commentToUpdate);
+    
+        return new Response<bool>(true, Success);
+    }
+    
+    public async Task<Response<bool>> DeleteAsync(int id)
+    {
+        Comment? comment = await commentRepository.GetByIdAsync(id);
+    
+        if (comment == null)
+        {
+            return new Response<bool>(false, NotFound);
+        }
+    
+        await commentRepository.DeleteAsync(comment);
+    
+        return new Response<bool>(true, Success);
+    }
+}
