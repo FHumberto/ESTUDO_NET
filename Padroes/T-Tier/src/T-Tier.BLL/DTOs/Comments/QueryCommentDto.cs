d707ce10e572572604d91fbb191ecf29c9585174
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-02-10 15:29:47 -0300

    [Padroes/T-Tier] Add: funcionalidade CRUD de comentários

---
+++ Padroes/T-Tier/src/T-Tier.BLL/DTOs/Comments/QueryCommentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace T_Tier.BLL.DTOs.Comments;
+
+public class QueryCommentDto
+{
+    public int Id { get; init; }
+    public int UserId { get; set; }
+    public int PostId { get; init; }
+    public string? Body { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; init; }
+}
--- Padroes/T-Tier/src/T-Tier.BLL/Services/CommentService.cs
+++ Padroes/T-Tier/src/T-Tier.BLL/Services/CommentService.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 using FluentValidation.Results;
 using T_Tier.BLL.DTOs.Comments;
-using T_Tier.BLL.Validators;
+using T_Tier.BLL.Validators.Comment;
 using T_Tier.BLL.Wrappers;
 using T_Tier.DAL.Contracts;
 using T_Tier.DAL.Entities;
@@ -15,73 +15,76 @@ public class CommentService(ICommentRepository commentRepository, IMapper mapper
     {
         IReadOnlyList<Comment> comments = await commentRepository.GetAllAsync();
         IReadOnlyList<QueryCommentDto> response = mapper.Map<IReadOnlyList<QueryCommentDto>>(comments);
-    
+
         return response == null || response.Count == 0
             ? new Response<IReadOnlyList<QueryCommentDto>>(new List<QueryCommentDto>(), NotFound)
             : new Response<IReadOnlyList<QueryCommentDto>>(response, Success);
     }
-    
+
     public async Task<Response<QueryCommentDto?>> GetCommentByIdAsync(int id)
     {
         Comment? comment = await commentRepository.GetByIdAsync(id);
         QueryCommentDto? response = mapper.Map<QueryCommentDto>(comment);
-    
+
         return response == null
             ? new Response<QueryCommentDto?>(null, NotFound)
             : new Response<QueryCommentDto?>(response, Success);
     }
 
-    public async Task<Response<int>> CreateCommentAsync(CommandCommentDto request)
+    public async Task<Response<int>> CreateCommentAsync(CreateCommentDto request)
     {
-        CommentValidator validationRules = new();
+        //TODO: Recuperar pelo Token o ID do usuário que está cadastrando o token.
+        CommentValidator validationRules = new(commentRepository);
         ValidationResult? validationResult = await validationRules.ValidateAsync(request);
-    
+
         if (!validationResult.IsValid)
         {
             return new Response<int>(0, InvalidInput, validationResult.Errors.Select(e => e.ErrorMessage).ToList());
         }
-    
+
         Comment commentToCreate = mapper.Map<Comment>(request);
         int createdCommentId = await commentRepository.CreateAsync(commentToCreate);
-    
+
         return new Response<int>(createdCommentId, Success);
     }
 
-    public async Task<Response<bool>> UpdateCommentAsync(CommandCommentDto request, int id)
+    public async Task<Response<bool>> UpdateCommentAsync(UpdateCommentDto request, int id)
     {
         Comment? commentToUpdate = await commentRepository.GetByIdAsync(id);
-    
+
         if (commentToUpdate == null)
         {
             return new Response<bool>(false, NotFound);
         }
-    
-        CommentValidator validationRules = new();
+
+        //TODO: Implementar regra para validar se o usuário for diferente do usuário que cadastrou
+        //TODO: Recuperar pelo Token o ID do usuário que está atualizando o token.
+        CommentUpdateValidator validationRules = new();
         ValidationResult? validationResult = await validationRules.ValidateAsync(request);
-    
+
         if (!validationResult.IsValid)
         {
             return new Response<bool>(false, InvalidInput, validationResult.Errors.Select(e => e.ErrorMessage).ToList());
         }
-    
+
         mapper.Map(request, commentToUpdate);
-    
+
         await commentRepository.UpdateAsync(commentToUpdate);
-    
+
         return new Response<bool>(true, Success);
     }
-    
+
     public async Task<Response<bool>> DeleteAsync(int id)
     {
         Comment? comment = await commentRepository.GetByIdAsync(id);
-    
+
         if (comment == null)
         {
             return new Response<bool>(false, NotFound);
         }
-    
+
         await commentRepository.DeleteAsync(comment);
-    
+
         return new Response<bool>(true, Success);
     }
 }
