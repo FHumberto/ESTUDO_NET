58a5dddbfbc5a190ccfcc6e57362248201f2eed5
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-02-22 12:26:01 -0300

    [Recursos/ConfigurationApi] Add: arquivos inciais do projeto

---
+++ Recursos/ConfigurationApi/Controllers/ConfigsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigurationApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ConfigsController : ControllerBase
+{
+    // GET
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
