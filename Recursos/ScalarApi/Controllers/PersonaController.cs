9e39f1ef16052210d125b7a3be108561627147a1
Humberto Guedes <franciscohguedes@gmail.com>
2025-02-27 06:56:12 -0300

    [Recursos] Add: arquivos iniciais do projeto ScalarApi

---
+++ Recursos/ScalarApi/Controllers/PersonaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScalarApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonaController : Controller
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { Name = "John Doe", Age = 30 });
+    }
+}
---
+++ Recursos/ScalarApi/Program.cs
@@ -0,0 +1,28 @@
+namespace ScalarApi;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
