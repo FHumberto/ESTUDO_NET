089b1fd0011639587b569e43e128feb9c10d1415
Humberto Guedes <franciscohguedes@gmail.com>
2025-02-28 07:10:06 -0300

    [Recursos] Misc: reorganização do diretório e adição de ScalarApiLabs

--- Recursos/ScalarApi/Data/Persona.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ScalarApi.Data;
-
-public class Persona
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public int Age { get; set; }
-}
---
+++ Recursos/ScalarApiLabs/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScalarApiLabs.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("Hello World!");
+    }
+}
