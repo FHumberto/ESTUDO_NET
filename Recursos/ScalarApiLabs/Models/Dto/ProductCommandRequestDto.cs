fc536954d0101ea671b40dce25bae2dee80be157
Humberto Guedes <franciscohguedes@gmail.com>
2025-02-28 12:18:37 -0300

    [Recursos/ScalarApiLabs] Add: funcionalidade de CRUD de Products usando RawSql para testes com Scalar

---
+++ Recursos/ScalarApiLabs/Models/Dto/ProductCommandRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScalarApiLabs.Models.Dto;
+
+public class ProductCommandRequestDto
+{
+    [Required(ErrorMessage = "O campo de nome é obrigatório.")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "O preço é obrigatório.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero.")]
+    public required decimal Price { get; set; }
+}
+
