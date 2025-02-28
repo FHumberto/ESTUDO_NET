411f95fd88517e592d1f01573a5195cf39a2a45a
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-02-28 08:04:54 -0300

    [Recursos/ScalarApiLabs] Add: entityframework ao projeto, migration, controlador e rota de get para testes da lib

---
+++ Recursos/ScalarApiLabs/Data/Migrations/20250228110019_InitialMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ScalarApiLabs.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Product 1", 10.00m },
+                    { 2, "Product 2", 20.00m },
+                    { 3, "Product 3", 30.00m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Recursos/ScalarApiLabs/Models/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace ScalarApiLabs.Models.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required decimal Price { get; set; }
+}
