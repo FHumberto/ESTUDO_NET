3e59b52aaa3876b35ce07de3d9ff0b83aa3f0ea9
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-04-26 08:09:24 -0300

    [Padroes/CARP] add: injeção de dependência e referência entre as camadas

---
+++ Padrões/CleanArchResultPattern/src/CARP.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CARP.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        => services;
+}
