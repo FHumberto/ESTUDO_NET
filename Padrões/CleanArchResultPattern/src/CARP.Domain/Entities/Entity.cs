1ecf5620330d9fed3f79adf2d705bd98a4b746af
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-04-26 12:50:01 -0300

    [Padroes/CARP] add: entidades de domínio e propriedades de navegação do entity framework

---
+++ Padrões/CleanArchResultPattern/src/CARP.Domain/Entities/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace CARP.Domain.Entities;
+
+public abstract class Entity
+{
+    public int Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime UpdatedAt { get; protected set; }
+}
