f794cd344d2747e9c28a0f53d80baa87075f9313
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-04-27 12:12:46 -0300

    [Padroes/CARP] add: abstração de entidade raiz, entidades de negócio e relacionamentos do entity framework

---
+++ Padrões/CARP/src/CARP.Domain/Categories/Category.cs
@@ -0,0 +1,22 @@
+﻿using CARP.Domain.Abstractions;
+using CARP.Domain.Products;
+
+namespace CARP.Domain.Categories;
+
+public sealed class Category : Entity
+{
+    public string? Name { get; private set; }
+
+    public ICollection<Product>? Products { get; set; }
+
+    public Category(string? name)
+    {
+        Name = name;
+    }
+
+    public Category(int id, string? name)
+    {
+        base.Id = id;
+        Name = name;
+    }
+}
