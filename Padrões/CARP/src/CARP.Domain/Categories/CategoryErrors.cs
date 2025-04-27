449db33fbb054d76fc2a88d555253f08d51d5d34
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-04-27 12:27:02 -0300

    [Padroes/CARP] add: classe de erro, e adiciona os possíveis erros de cada entidade

---
+++ Padrões/CARP/src/CARP.Domain/Categories/CategoryErrors.cs
@@ -0,0 +1,21 @@
+﻿using CARP.Domain.Abstractions;
+
+namespace CARP.Domain.Categories;
+
+public static class CategoryErrors
+{
+    public static readonly Error CategoryNotFound =
+        new("Error.CategoryNotFound", "Categoria não encontrada.");
+
+    public static readonly Error CategoryAlreadyExists =
+        new("Error.CategoryAlreadyExists", "Categoria já existe.");
+
+    public static readonly Error CategoryNameIsNullOrEmpty =
+        new("Error.CategoryNameIsNullOrEmpty", "O nome da categoria está nulo ou vazio.");
+
+    public static readonly Error CategoryNameTooShort =
+        new("Error.CategoryNameTooShort", "O nome da categoria é menor que 3 caracteres.");
+
+    public static readonly Error CategoryNameTooLong =
+        new("Error.CategoryNameTooLong", "O nome da categoria é muito longo.");
+}
---
+++ Padrões/CARP/src/CARP.Domain/Products/ProductErrors.cs
@@ -0,0 +1,33 @@
+﻿using CARP.Domain.Abstractions;
+
+namespace CARP.Domain.Products;
+
+public static class ProductErrors
+{
+    public static readonly Error ProductNotFound =
+        new("Error.ProductNotFound", "Produto não encontrado.");
+
+    public static readonly Error ProductAlreadyExists =
+        new("Error.ProductAlreadyExists", "O produto já existe.");
+
+    public static readonly Error ProductNameIsNullOrEmpty =
+        new("Error.ProductNameIsNullOrEmpty", "O nome do produto está nulo ou vazio.");
+
+    public static readonly Error ProductNameTooLong =
+        new("Error.ProductNameTooLong", "O nome do produto é muito longo.");
+
+    public static readonly Error ProductDescriptionIsNullOrEmpty =
+        new("Error.ProductDescriptionIsNullOrEmpty", "A descrição do produto está nula ou vazia.");
+
+    public static readonly Error ProductPriceIsInvalid =
+        new("Error.ProductPriceIsInvalid", "O preço do produto é inválido.");
+
+    public static readonly Error ProductStockIsInvalid =
+        new("Error.ProductStockIsInvalid", "A quantidade em estoque do produto é inválida.");
+
+    public static readonly Error ProductCategoryNotFound =
+        new("Error.ProductCategoryNotFound", "A categoria do produto não foi encontrada.");
+
+    public static readonly Error ProductCategoryIsNull =
+        new("Error.ProductCategoryIsNull", "A categoria do produto está nula.");
+}
