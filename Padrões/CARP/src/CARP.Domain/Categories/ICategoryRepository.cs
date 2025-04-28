9ab934986af838b74a46d3893f179b3442f30bd8
Humberto Guedes <7258900+FHumberto@users.noreply.github.com>
2025-04-28 04:45:20 -0300

    [Padroes/CARP] add: assinaturas de métodos de persistência

---
+++ Padrões/CARP/src/CARP.Domain/Categories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace CARP.Domain.Categories;
+
+public interface ICategoryRepository
+{
+    Task<Category> GetCategoryByIdAsync(Guid id);
+    Task<IEnumerable<Category>> GetAllCategoriesAsync();
+    Task AddCategoryAsync(Category category);
+    Task UpdateCategoryAsync(Category category);
+    Task DeleteCategoryAsync(Guid id);
+}
---
+++ Padrões/CARP/src/CARP.Domain/Products/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace CARP.Domain.Products;
+
+public interface IProductRepository
+{
+    Task<Product> GetProductByIdAsync(Guid id);
+    Task<IEnumerable<Product>> GetAllProductsAsync();
+    Task AddProductAsync(Product product);
+    Task UpdateProductAsync(Product product);
+    Task DeleteProductAsync(Guid id);
+}
